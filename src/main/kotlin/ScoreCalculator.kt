86e5035e60781ca7da79f3b50f5e0767df0fbe70
Rindra R. <rindra.randriambololona@gmail.com>
2023-10-04 18:11:24 +0200

    feat(score) : refactor simple case

--- src/main/kotlin/ScoreCalculator.kt
+++ src/main/kotlin/ScoreCalculator.kt
@@ -1,11 +1,8 @@
 class ScoreCalculator {
 
     fun compute(cases: String): Int {
-        return when (cases) {
-            "9-9-9-9-9-9-9-9-9-9-" -> 90
-            "1-1-1-1-1-1-1-1-1-1-" -> 10
-            "1-2-3-4-5-6-7-8-9-1-" -> 46
-            else -> 0
-        }
+        return cases.toCharArray()
+            .filter { it.isDigit() }
+            .sumOf { it.digitToInt() }
     }
 }
